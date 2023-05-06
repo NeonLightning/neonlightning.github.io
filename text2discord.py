1ab45263490e063ac6bfc26cad7edb577f8faac4
NeonLightning <neonlightning@gmail.com>
2023-05-06 15:29:06 -0400

    Add files via upload

--- text2discord.py
+++ text2discord.py
@@ -1,65 +1,67 @@
+'''
+Text2Discord
+Sends the output of specific text files out to Discord. for "logs..."
+just gotta set main.plugins.text2discord.files and main.plugins.text2discord.webhook
+'''
+
+
 import logging
 import requests
+import pwnagotchi
 import pwnagotchi.plugins as plugins
 import os
+import time
 
 class Text2Discord(plugins.Plugin):
     __author__ = 'NeonLightning'
-    __version__ = '0.0.2'
+    __version__ = '0.0.3'
     __license__ = 'GPL3'
     __description__ = '''
-                    Sends the output of specific text files line by line out to Discord.
+                    Sends the full content of specific text files to Discord, split into multiple messages if necessary.
                     '''
 
     def __init__(self):
         logging.debug("[*] Text2Discord plugin created")
-        self.last_line_numbers = {}
-
-    def process_file(self, file_path):
-        try:
-            if not os.path.exists(file_path):
-                logging.warning(f"File not found: {file_path}")
-                return
-
-            with open(file_path, 'r') as f:
-                lines = f.readlines()
-                logging.debug(f"Sending {len(lines)} lines from {file_path}")
-                webhook_url = pwnagotchi.config['main']['plugins']['text2discord']['webhook']
-                self.send_lines(lines, webhook_url)
-        except Exception as e:
-            logging.warning(f"An error occurred while processing the file: {file_path}. Error: {str(e)}")
-
-    def send_lines(self, lines, webhook_url):
-        message = ''
-        for i, line in enumerate(lines):
-            if len(message) + len(line) > 2000:
-                data = {'content': message}
-                response = requests.post(webhook_url, json=data)
-                if response.status_code != 204:
-                    logging.warning(f"Failed to send message to Discord: {message.strip()}")
-                message = line
-            elif i + 1 < len(lines) and len(message) + len(lines[i+1]) > 2000:
-                data = {'content': message + line}
-                response = requests.post(webhook_url, json=data)
-                if response.status_code != 204:
-                    logging.warning(f"Failed to send message to Discord: {message.strip()}")
-                message = ''
-            else:
-                message += line
-        if len(message) > 0:
-            data = {'content': message}
-            response = requests.post(webhook_url, json=data)
-            if response.status_code != 204:
-                logging.warning(f"Failed to send message to Discord: {message.strip()}")
+        self.internet_on = False
 
     def on_loaded(self):
+        logging.info("Text2Discord loaded")
         self.file_paths = pwnagotchi.config['main']['plugins']['text2discord']['files']
-        for file_path in self.file_paths:
-            self.last_line_numbers[file_path] = 0
+        self.webhook_url = pwnagotchi.config['main']['plugins']['text2discord']['webhook']
+        logging.debug(f"File paths: {self.file_paths}")
+        self.last_line_numbers = {file_path: 0 for file_path in self.file_paths}
+        self.main_loop()
 
-        for file_path in self.file_paths:
-            self.process_file(file_path)
-
-        logging.info(f"[*] Text2Discord plugin done")
-
-
+    def main_loop(self):
+        while True:
+            for file_path in self.file_paths:
+                try:
+                    if not os.path.exists(file_path):
+                        logging.warning(f"File not found: {file_path}")
+                        continue
+                    with open(file_path, 'r') as f:
+                        lines = f.readlines()
+                        current_line_number = len(lines)
+                        if current_line_number != self.last_line_numbers[file_path]:
+                            logging.debug(f"Sending {current_line_number} lines from {file_path}")
+                            messages = []
+                            msg = ''
+                            for line in lines[self.last_line_numbers[file_path]:]:
+                                msg += line
+                                if len(msg) >= 1650:
+                                    messages.append(msg)
+                                    msg = ''
+                            if msg:
+                                messages.append(msg)
+                            for i, msg in enumerate(messages):
+                                data = {'content': f'{file_path}\n{msg}'}
+                                webhook_url = pwnagotchi.config['main']['plugins']['text2discord']['webhook']
+                                response = requests.post(webhook_url, json=data)
+                                if response.status_code != 204:
+                                    logging.warning(f"Failed to send message to Discord: {msg.strip()}")
+                                else:
+                                    logging.info(f"Sent message {i + 1} from {file_path}")
+                            self.last_line_numbers[file_path] = current_line_number
+                except Exception as e:
+                    logging.warning(f"An error occurred while processing the file: {file_path}. Error: {str(e)}")
+            time.sleep(3600)
